4a750eaadb8d447aa1622a3864c1eb119202a229
Adedoyin Famuyiwa <nightanime13@gmail.com>
2017-04-27 17:47:10 +0000

    added database questions

--- app/database.sql
+++ app/database.sql
@@ -12,3 +12,12 @@ CREATE TABLE Answers (
 );
 
 INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I am the home to the worlds largest building.', 'AX', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I produce the most oil.', 'SA', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have the most time zones.', 'FR', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have the most billionaires.', 'US', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have the best econnomy.', 'CH', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
