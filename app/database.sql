3815ecb290a7c6e8e1175270ef40534c7e01cec3
Adedoyin Famuyiwa <nightanime13@gmail.com>
2017-04-27 19:10:57 +0000

    create login page

--- app/database.sql
+++ app/database.sql
@@ -2,22 +2,36 @@ DROP DATABASE IF EXISTS geopardy;
 
 CREATE DATABASE geopardy;
 
+USE geopardy;
+
 CREATE TABLE Answers (
     answerID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    answerText VAR_CHAR(500) NOT NULL,
-    country VAR_CHAR(2) NOT NULL,
+    answerText VARCHAR(500) NOT NULL,
+    country VARCHAR(2) NOT NULL,
     difficulty INT NOT NULL,
     totalCount INT NOT NULL,
     correctCount INT NOT NULL
 );
 
+CREATE TABLE Users (
+    userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(500) NOT NULL,
+    password VARCHAR(500) NOT NULL
+);
+
+CREATE TABLE Scores (
+    name VARCHAR(10) NOT NULL DEFAULT 'Anonymous' PRIMARY KEY,
+    score INT(5) UNSIGNED NOT NULL DEFAULT '0',
+    ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
 INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
-INSERT INTO Answers VALUES( null, 'I am the home to the worlds largest building.', 'AX', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I am the home to the worlds largest building.', 'AE', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I produce the most oil.', 'SA', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I have the most time zones.', 'FR', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I have the most billionaires.', 'US', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I have the best econnomy.', 'CH', 1, 0, 0);
-INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
-INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
-INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
-INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have the longest street.', 'CA', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have invented french fires.', 'BE', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I am the only country to be named after a tree.', 'BR', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I consume more coca-cola than any other country.', 'IS', 1, 0, 0);
