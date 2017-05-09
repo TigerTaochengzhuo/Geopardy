5101b6a8fc9c002bd8becad681373401375ac049
Adedoyin Famuyiwa <nightanime13@gmail.com>
2017-05-09 18:01:37 +0000

    updated application

--- app/database.sql
+++ app/database.sql
@@ -25,13 +25,15 @@ CREATE TABLE Scores (
     ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+
+
 INSERT INTO Answers VALUES( null, 'I have the longest wall.', 'CN', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I am the home to the worlds largest building.', 'AE', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I produce the most oil.', 'SA', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I have the most time zones.', 'FR', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I have the most billionaires.', 'US', 1, 0, 0);
-INSERT INTO Answers VALUES( null, 'I have the best econnomy.', 'CH', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have the best economy.', 'CH', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I have the longest street.', 'CA', 1, 0, 0);
-INSERT INTO Answers VALUES( null, 'I have invented french fires.', 'BE', 1, 0, 0);
+INSERT INTO Answers VALUES( null, 'I have invented french fries.', 'BE', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I am the only country to be named after a tree.', 'BR', 1, 0, 0);
 INSERT INTO Answers VALUES( null, 'I consume more coca-cola than any other country.', 'IS', 1, 0, 0);
